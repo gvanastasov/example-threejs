bbf75e9c0274952a61074549ad0c5635018d40e6
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2024-04-08 21:58:43 +0200

    add texture example

--- src/scenes/02_models/index.js
+++ src/scenes/02_models/index.js
@@ -3,8 +3,6 @@ import { GLTFLoader  } from "three/examples/jsm/Addons.js";
 
 const loader = new GLTFLoader();
 
-let url = new URL( './model.gltf', import.meta.url );
-
 const loaderExample = function() {
     this.name = 'Loader',
     this.group = 'Models',
@@ -15,7 +13,7 @@ const loaderExample = function() {
         model: null,
     }
     this.start = (scene) => {
-        loader.load(url.toString(), ( gltf ) => {
+        loader.load('static/cybertruck.gltf', ( gltf ) => {
             const material = new THREE.MeshPhongMaterial({color: 0xFF0000});
             const model = gltf.scene.children.find( child => child.name === 'Tesla' );
             model.traverse( function ( child ) {
@@ -40,4 +38,46 @@ const loaderExample = function() {
     return this;
 }
 
-export default [ new loaderExample() ];
+const textureExample = function() {
+    this.name = 'Texture',
+    this.group = 'Models',
+    this.refs = {
+        /**
+         * @type {THREE.Mesh}
+         */
+        model: null,
+    }
+    this.start = (scene) => {
+        loader.load('static/crate/scene.gltf', ( gltf ) => {
+            const model = gltf.scene.children[0];
+            if (!model) {
+                return;
+            }
+            model.scale.set(2, 2, 2);
+            model.traverse( function ( child ) {
+                if ( child.isMesh ) {
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                }
+            });
+            scene.add(model);
+            this.refs.model = model;
+        }, undefined, function ( error ) {
+            console.error( error );
+        } );
+    }
+    this.update = () => {
+        if (this.refs.model) {
+            this.refs.model.rotateOnAxis(new THREE.Vector3(0,0,1), 0.01);
+        }
+    }
+    this.onSceneGUI = (scene, rootElement) => {
+        const licenseMarkup = '<p>This work is based on <a href="https://sketchfab.com/3d-models/crate-15581c596def402db1196f92de5c5757">"Crate"</a> by <a href="https://sketchfab.com/cooper">cooper</a>.</p>';
+        const licenseDiv = document.createElement('div');
+        licenseDiv.innerHTML = licenseMarkup;
+        rootElement.appendChild(licenseDiv);
+    }
+    return this;
+}
+
+export default [ new loaderExample(), new textureExample()];
